a2b2277c488729b48e0eae879741e721d9260201
fbastier <francois.bastier@gmail.com>
2021-06-21 14:14:44 +0200

    Injection avec scope

---
+++ src/main/java/com/example/springapp/Model/Service.java
@@ -0,0 +1,15 @@
+package com.example.springapp.Model;
+
+public class Service {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
--- src/main/java/com/example/springapp/ProjetDemoApplication.java
+++ src/main/java/com/example/springapp/ProjetDemoApplication.java
@@ -3,6 +3,7 @@ package com.example.springapp;
 import com.example.springapp.Model.Compagny;
 import com.example.springapp.Model.Developper;
 import com.example.springapp.Model.House;
+import com.example.springapp.Model.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -36,6 +37,32 @@ public class ProjetDemoApplication {
         System.out.println(compagny);
         System.out.println(developper1);
         System.out.println(developper2);
+
+
+        // Injection avec utilisation d'un scope
+        // singleton
+        System.out.println("Injection avec utilisation d'un scope");
+        System.out.println("Avec singleton");
+
+        Service service = (Service) cxt.getBean("service");
+        service.setMessage("Réalisé avec scope singleton");
+        System.out.println(service.getMessage());
+
+        Service service1 = (Service) cxt.getBean("service");
+        System.out.println(service1.getMessage());
+
+
+        // prototype
+        System.out.println("Injection avec utilisation d'un scope");
+        System.out.println("Avec prototype");
+
+        Service service2 = (Service) cxt.getBean("service1");
+        service2.setMessage("Réalisé avec scope prototype");
+        System.out.println(service2.getMessage());
+
+        Service service3 = (Service) cxt.getBean("service1");
+        System.out.println(service3.getMessage());
+
 
 
     }
